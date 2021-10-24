e648d5dd3f58e7bd4256b744cc3911c1b40ba86f
Junoburger <juno.burger@pm.me>
2021-10-24 23:43:19 +0200

    Correct sentence

--- bmimetric2.py
+++ bmimetric2.py
@@ -5,15 +5,15 @@ height = float(input("Please enter height in meters: "))
 BMI = round(weight / (height * height), 2)
 
 if BMI < 18.5:
-    status = "Underweight"
+    status = "underweight"
 elif BMI > 18.5 and BMI <= 24.9:
-    status = "Normal"
+    status = "normal"
 elif BMI >= 25 and BMI <= 29.9:
-    status = "Overweight"
+    status = "overweight"
 elif BMI >= 30.0:
-    status = "Obese"
+    status = "obese"
 
 else:
     status = "Unknown outcome"
 
-print("BMI is:", BMI, ", status is", status)
+print("BMI is: ", BMI, ", status is ", status, sep="")
